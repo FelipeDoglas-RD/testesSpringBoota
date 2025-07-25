91f8738ebdf80b4396f393f75aee9f7c68d0f49b
[Felipe Doglas] <[felipedoglasmhw@gmail.com]>
2025-07-25 14:17:21 -0300

    first commit

---
+++ src/main/java/dev/teste/testeCadastros/TesteCadastrosApplication.java
@@ -0,0 +1,13 @@
+package dev.teste.testeCadastros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCadastrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCadastrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/teste/testeCadastros/TesteCadastrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.teste.testeCadastros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCadastrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
