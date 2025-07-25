50191846ab33a301bdd37cab6c6858e9863918cb
[Felipe Doglas] <[felipedoglasmhw@gmail.com]>
2025-07-25 15:06:49 -0300

    Created a controller[aula 2]

--- src/main/java/dev/teste/testeCadastros/Controler.java
+++ src/main/java/dev/teste/testeCadastros/Controler.java
@@ -1,4 +1,12 @@
 package dev.teste.testeCadastros;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class Controler {
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Primeira Mensagem";
+    }
 }
